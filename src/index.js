2108622a52ba69939ff23f0af58187d22f7ac8ad
gmoyatorres <gmoyatorres@hfmi.com>
2016-12-13 12:55:40 -0400

    Added functionality to chunk 1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import {render} from 'react-dom';
 
 // those chunks will be moved to folders in comming steps
 class Chunk1 extends Component {
+  constructor(props){
+    super(props);
+      this.state = {chuck2: []};
+      this.pressed = this.pressed.bind(this);
+  }
+
+  pressed (e) {
+    this.setState({chuck2: this.getChunk()});
+  }
+
+  getChunk(){
+    return (
+      [<Chunk2 key={0} />]
+    )
+  }
 
   render(){
     return (
-      <div> <button> Click here to open the other chunk </button> </div>
+      <div> <button onClick={this.pressed}> Click here to open the other chunk </button> {this.state.chuck2} </div>
     );
   }
 
